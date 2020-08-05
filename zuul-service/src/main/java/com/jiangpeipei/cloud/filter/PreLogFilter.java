bebe0960350bb4fb1f9d789563dc1c4baff1bc90
jiangpeipei <jiangpeipei@qutoutiao.net>
2020-08-05 15:25:29 +0800

    add zuul service

---
+++ zuul-service/src/main/java/com/jiangpeipei/cloud/filter/PreLogFilter.java
@@ -0,0 +1,40 @@
+package com.jiangpeipei.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Component
+public class PreLogFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String host = request.getRemoteHost();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        log.info("Remote host:{},method:{},uri:{}", host, method, uri);
+        return null;
+    }
+}
